eab8bcf2f37c8d8b06958cc837c6b6d7b6af7c99
Roger Leigh <rleigh@debian.org>
2007-12-28 01:09:57 +0000

    * Update copyright and GPL2+ licence boilerplate on all files.

--- lib/Sbuild/Utility.pm
+++ lib/Sbuild/Utility.pm
@@ -2,22 +2,21 @@
 # Utility.pm: library for sbuild utility programs
 # Copyright © 2006 Roger Leigh <rleigh@debian.org>
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 #
-# $Id: Sbuild.pm,v 1.2 2006/03/07 16:58:12 rleigh Exp $
-#
+############################################################################
 
 # Import default modules into main
 package main;
