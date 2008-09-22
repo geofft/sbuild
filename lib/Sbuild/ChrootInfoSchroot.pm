98f792924fd6e6392e9f2d4d175547947f4edc18
Roger Leigh <rleigh@debian.org>
2008-09-23 00:35:13 +0100

    [Sbuild::ChrootInfoSchroot, Sbuild::ChrootInfoSudo] Add new subclasses of Sbuild::ChrootInfo

---
+++ lib/Sbuild/ChrootInfoSchroot.pm
@@ -0,0 +1,49 @@
+#
+# ChrootInfo.pm: chroot utility library for sbuild
+# Copyright © 2005-2006 Roger Leigh <rleigh@debian.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+#######################################################################
+
+package Sbuild::ChrootInfoSchroot;
+
+use Sbuild::ChrootInfo;
+
+use strict;
+use warnings;
+
+sub new ($$);
+
+BEGIN {
+    use Exporter ();
+    our (@ISA, @EXPORT);
+
+    @ISA = qw(Exporter Sbuild::ChrootInfo);
+
+    @EXPORT = qw();
+}
+
+sub new ($$) {
+    my $class = shift;
+    my $conf = shift;
+
+    my $self = $class->SUPER::new($conf);
+    bless($self, $class);
+
+    return $self;
+}
+
+1;
