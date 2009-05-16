2c6f4d19b5a33cfc286f244f6377ffd64437f21e
Roger Leigh <rleigh@debian.org>
2009-05-17 00:35:31 +0100

    [buildd] Execute commands using Sbuild::Chroot interface

--- lib/Sbuild/ChrootRoot.pm
+++ lib/Sbuild/ChrootRoot.pm
@@ -1,7 +1,7 @@
 #
-# ChrootRoot.pm: chroot access via root filesystem
+# ChrootRoot.pm: Run commands on the root filesystem
 # Copyright © 2005      Ryan Murray <rmurray@debian.org>
-# Copyright © 2005-2006 Roger Leigh <rleigh@debian.org>
+# Copyright © 2005-2009 Roger Leigh <rleigh@debian.org>
 # Copyright © 2008      Simon McVittie <smcv@debian.org>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -20,20 +20,19 @@
 #
 #######################################################################
 
-package Sbuild::ChrootPlain;
+package Sbuild::ChrootRoot;
 
 use strict;
 use warnings;
 
-use Sbuild::Log;
-use Sbuild::Sysconfig;
+use Sbuild::Conf;
+use Sbuild::ChrootPlain;
 
 BEGIN {
     use Exporter ();
-    use Sbuild::Chroot;
     our (@ISA, @EXPORT);
 
-    @ISA = qw(Exporter Sbuild::Chroot);
+    @ISA = qw(Exporter Sbuild::ChrootPlain);
 
     @EXPORT = qw();
 }
@@ -41,9 +40,8 @@ BEGIN {
 sub new {
     my $class = shift;
     my $conf = shift;
-    my $chroot_id = shift;
 
-    my $self = $class->SUPER::new($conf, $chroot_id);
+    my $self = $class->SUPER::new($conf, '/');
     bless($self, $class);
 
     # Only run split, because plain makes no guarantee that networking
@@ -51,93 +49,6 @@ sub new {
     $self->set('Split', 1);
 
     return $self;
-}
-
-sub begin_session {
-    my $self = shift;
-
-    $self->set('Priority', 0);
-    $self->set('Location', $self->get('Chroot ID'));
-    $self->set('Session Purged', 0);
-
-    $self->_setup_options();
-
-    return 1;
-}
-
-sub end_session {
-    my $self = shift;
-
-    # No-op.
-
-    return 1;
-}
-
-sub get_command_internal {
-    my $self = shift;
-    my $options = shift;
-
-    my $command = $options->{'INTCOMMAND'}; # Command to run
-    my $user = $options->{'USER'};          # User to run command under
-    my $chroot = $options->{'CHROOT'};      # Run in chroot?
-    my $dir;                                # Directory to use (optional)
-    $dir = $self->get('Defaults')->{'DIR'} if
-	(defined($self->get('Defaults')) &&
-	 defined($self->get('Defaults')->{'DIR'}));
-    $dir = $options->{'DIR'} if
-	defined($options->{'DIR'}) && $options->{'DIR'};
-
-    if (!defined $user || $user eq "") {
-	$user = $self->get_conf('USERNAME');
-    }
-    if (!defined $chroot) {
-	$chroot = 1;
-    }
-
-    my @cmdline;
-    my $chdir = undef;
-    if ($chroot != 0) { # Run command inside chroot
-	if (!defined($dir)) {
-	    $dir = '/';
-	}
-
-	my $shellcommand;
-	foreach (@$command) {
-	    my $tmp = $_;
-	    $tmp =~ s/'//g; # Strip any single quotes for security
-	    if ($_ ne $tmp) {
-		$self->log_warning("Stripped single quote from command for security: $_\n");
-	    }
-	    if ($shellcommand) {
-		$shellcommand .= " '$tmp'";
-	    } else {
-		$shellcommand = "'$tmp'";
-	    }
-	}
-
-	@cmdline = ('/usr/sbin/chroot', $self->get('Location'),
-		    $self->get_conf('SU'), '-p', "$user", '-s',
-		    $Sbuild::Sysconfig::programs{'SHELL'}, '-c',
-		    "cd '$dir' && $shellcommand");
-    } else { # Run command outside chroot
-	if ($options->{'CHDIR_CHROOT'}) {
-	    my $tmpdir = $self->get('Location');
-	    $tmpdir = $tmpdir . $dir if defined($dir);
-	    $dir = $tmpdir;
-	}
-	if ($user ne $self->get_conf('USERNAME')) {
-	    print main::LOG "Command \"$command\" cannot be run as user $user on the host system\n";
-	}
-	$chdir = $dir if defined($dir);
-	push(@cmdline, @$command);
-    }
-
-    $options->{'CHROOT'} = $chroot;
-    $options->{'USER'} = $user;
-    $options->{'COMMAND'} = $command;
-    $options->{'EXPCOMMAND'} = \@cmdline;
-    $options->{'CHDIR'} = $chdir;
-    $options->{'DIR'} = $dir;
 }
 
 1;
