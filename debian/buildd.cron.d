8955f1532f246fb78180d0f22d7196ce21bd47a1
Andreas Barth <aba@not.so.argh.org>
2010-04-04 10:25:50 +0200

    cron: buildd watcher runs every 15 minutes

--- debian/buildd.cron.d
+++ debian/buildd.cron.d
@@ -1,4 +1,4 @@
 # crontab for buildd
 # min        hour         day mon wday user     cmd
 10,25,40,55  *            *   *   *    buildd   /usr/bin/buildd-uploader
-20           *            *   *   *    buildd   /usr/bin/buildd-watcher
+5,20,35,50   *            *   *   *    buildd   /usr/bin/buildd-watcher
