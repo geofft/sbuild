fb790792a9433d469fa808f6613bdc0bf1f3ee21
Roger Leigh <rleigh@debian.org>
2010-12-30 17:53:15 +0000

    Sbuild::ResolverBase: Don't automatically generate archive signing key
    If the local archive signing key is not present, do not automatically
    generate it.  This can cause issues on systems with scarce entropy.
    Print an error message plus instructions on how to generate the key.

--- lib/Sbuild/ResolverBase.pm
+++ lib/Sbuild/ResolverBase.pm
@@ -758,17 +758,15 @@ sub generate_keys {
         return 1;
     }
 
-    my $host = $self->get('Host');
-
-    $self->log("Generating GPG local archive signing key...\n");
-    if (Sbuild::ChrootSetup::generate_keys($host, $self->get('Config'))) {
-	# Since apt-distupgrade was requested specifically, fail on
-	# error when not in buildd mode.
-	$self->log("Generating gpg keys failed\n");
-	return 0;
-    }
-
-    return 1;
+    $self->log_error("Local archive GPG signing key not found\n");
+    $self->log_info("Please generate a key with 'sbuild-update --keygen'\n");
+    $self->log_info("Note that on machines with scarce entropy, you may wish ".
+		    "to generate the key with this command on another machine ".
+		    "and copy the public and private keypair to '" .
+		    $self->get_conf('SBUILD_BUILD_DEPENDS_PUBLIC_KEY')
+		    ."' and '".
+		    $self->get_conf('SBUILD_BUILD_DEPENDS_SECRET_KEY') ."'\n");
+    return 0;
 }
 
 # Function that runs apt-ftparchive
